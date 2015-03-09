18a1b191e1d540c8cb3be5ef43b0ec90d8b3bb5a
Aditya Dhulipala <adhulipa@usc.edu>
2015-03-08 19:33:51 -0700

    fixed makedile

--- phw4/p1_section.c
+++ phw4/p1_section.c
@@ -1,16 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <omp.h>
 
 #define		num_of_points	   40000000
+
 typedef struct{
 	double x;  
 	double y;
 }Point; 
 
 int main(void){
-	int i;
+	int i,tid;
 	int num_of_points_in_circle;
+	int chunk;
+	
 	double pi;
 	struct timespec start, stop; 
 	double time;
@@ -25,16 +29,27 @@ int main(void){
 	
 	
 	////////**********Use OpenMP to parallize this loop***************//
-	for(i=0; i<num_of_points; i++){
-		if((data_point[i].x-0.5)*(data_point[i].x-0.5)+(data_point[i].y-0.5)*(data_point[i].y-0.5)<=0.25){
-			num_of_points_in_circle++;
-		}	
-	}	
+	omp_set_num_threads(4);
+	chunk = num_of_points/4;
+	printf("%d\n", chunk);
+	#pragma omp parallel shared(data_point, num_of_points_in_circle) private(i,tid)
+	{
+		#pragma omp for schedule(static, chunk) reduction(+:num_of_points_in_circle)
+		for(i=0; i<num_of_points; i++){
+		tid = omp_get_thread_num();
+			if((data_point[i].x-0.5)*(data_point[i].x-0.5)
+			+(data_point[i].y-0.5)*(data_point[i].y-0.5)
+			<=0.25) {
+				num_of_points_in_circle++;
+			}	
+		}
+	
+	}
 	///////******************************////
 	
 	if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}		
 	time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
 		
 	pi =4*(double)num_of_points_in_circle/(double)num_of_points;
-	printf("Estimated pi is %f, execution time = %f sec\n",  pi, time);		
+	printf("Estimated pi is %f, execution time = %f sec\n",  pi, time);
 }	
