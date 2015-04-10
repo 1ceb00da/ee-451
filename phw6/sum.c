9c7990e1a9e1a32c666b47b48e6338ee4e28370d
Aditya Dhulipala <aditya.d@hotmail.com>
2015-04-10 01:30:45 -0700

    sarted p2

---
+++ phw6/sum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<mpi.h>
+
+int main(int a, char **argv) {
+
+	FILE *fp;
+	
+	
+	
+	fp = fopen("numbers.txt");
+	if(fp == NULL) {
+		printf("file open error\n");
+	}
+	while (fgets(a, )) {
+
+	}
+
+
+}
